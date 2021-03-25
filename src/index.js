ea3deaf2d8567a93d11348424861cbbdc910651c
Wesley <wes.beck21@outlook.com>
2021-03-25 09:38:09 -0700

    commit tree d1e5409e671eb4034c2ffc592804ed68fc0e354b

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert('I am just happy')} />
   </div>,
   document.getElementById('root')
 );
